669b50b6810d794d3d1a4eda323945243fbf2121
SiddharthSai4701 <67595252+SiddharthSai4701@users.noreply.github.com>
2021-10-06 16:22:00 +0530

    Update and rename 1Complex Numbers.py to Complex Numbers.py

--- Complex Numbers.py
+++ Complex Numbers.py
@@ -13,8 +13,8 @@ y = int(input("Enter the imaginary value: "))
 
 def comp(x=0,y=0):
     print("The complex number is",complex(x,y))   # The complex number
-    print("The complex conjugate is",complex(x,-y))
-    print("The modulus is",int(m.pow((x**2+y**2),0.5)))
-    print("The argument is",m.atan(y/x))
+    print("The complex conjugate is",complex(x,-y)) # The conjugate of the complex number
+    print("The modulus is",int(m.pow((x**2+y**2),0.5))) # The modulus of the complex number
+    print("The argument is",m.atan(y/x)) # The argument of the complex number
     
 comp(x,y)
